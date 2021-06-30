3a3dfb5d3c06925303a62120202e67aeb961adfb
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-30 05:20:01 -0400

    Inversion de contrôle
    code plus flexible et court, la class app restera dès à présent inchangé pour toutes modifications ou ajouts des composants grâce l'utilisation de la reflexivité de Java

--- invoice/src/main/java/com/mycompany/invoice/App.java
+++ invoice/src/main/java/com/mycompany/invoice/App.java
@@ -1,86 +1,47 @@
 package com.mycompany.invoice;
 
 import com.mycompany.invoice.controller.InvoiceControllerInterface;
-import com.mycompany.invoice.controller.InvoiceControllerKeyboard;
-import com.mycompany.invoice.controller.InvoiceControllerDouchette;
-import com.mycompany.invoice.controller.InvoiceControllerWeb;
 import com.mycompany.invoice.repository.InvoiceRepositoryInterface;
-import com.mycompany.invoice.repository.InvoiceRepositoryMemory;
-import com.mycompany.invoice.repository.InvoiceRepositoryDatabase;
 import com.mycompany.invoice.service.InvoiceServiceInterface;
-import com.mycompany.invoice.service.InvoiceServiceNumber;
-import com.mycompany.invoice.service.InvoiceServicePrefix;
 
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Classe de lancement d'Application
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Quel est le type de controller ? (Keyboard, web, Douchette)");
-        String controllerType = scan.nextLine();
-        System.out.println("Quel est le type de service ? (number, prefix)");
-        String serviceType = scan.nextLine();
-        System.out.println("Quel est le type de repository ? (memory, Database)");
-        String repositoryType = scan.nextLine();
 
-
-
-        InvoiceControllerInterface invoiceController = null;
-        switch (controllerType.toLowerCase()){
-            case "keyboard" :
-                invoiceController = new InvoiceControllerKeyboard();
-                break;
-            case "web":
-                invoiceController = new InvoiceControllerWeb();
-                break;
-            case "douchette":
-                invoiceController = new InvoiceControllerDouchette();
-                break;
-        }
-
-        InvoiceServiceInterface invoiceService = null;
-
-        switch(serviceType.toLowerCase()){
-            case "number":
-                invoiceService = new InvoiceServiceNumber();
-                break;
-            case "prefix":
-                invoiceService = new InvoiceServicePrefix();
-                break;
-        }
-        InvoiceRepositoryInterface invoiceRepository = null;
-
-        switch(repositoryType.toLowerCase()){
-            case "memory":
-                invoiceRepository = new InvoiceRepositoryMemory();
-                break;
-            case "database":
-                invoiceRepository = new InvoiceRepositoryDatabase();
-                break;
-        }
-
-        invoiceController.setInvoiceService(invoiceService);
-        invoiceService.setInvoiceRepository(invoiceRepository);
-        invoiceController.createInvoice();
-    }
-}
-     /* System.out.println("Quel est la Classe de controller ? ");
+        System.out.println("Quel est la Classe de controller ? ");
         String controllerClass = scan.nextLine();
         System.out.println("Quel est la Classe de service ?");
         String serviceClass = scan.nextLine();
         System.out.println("Quel est la Classe de repository ?");
-        String repositoryClass = scan.nextLine();*/
+        String repositoryClass = scan.nextLine();
 
- /*  try {
+        // Initialisation des interfaces des composants
+        InvoiceControllerInterface invoiceController = null;
+        InvoiceServiceInterface invoiceService = null;
+        InvoiceRepositoryInterface invoiceRepository = null;
+
+        // instanciation des composants grâce à la reflection de Java
+        try {
             invoiceController = (InvoiceControllerInterface) Class.forName(controllerClass).getDeclaredConstructor().newInstance();
             invoiceService = (InvoiceServiceInterface) Class.forName(serviceClass).getDeclaredConstructor().newInstance();
             invoiceRepository = (InvoiceRepositoryInterface) Class.forName(repositoryClass).getDeclaredConstructor().newInstance();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
+
+        // Démarrer les services tout en faisant l'injection des dépendances entre les composants
+        if (invoiceController != null && invoiceService != null) {
+            invoiceController.setInvoiceService(invoiceService);
+            invoiceService.setInvoiceRepository(invoiceRepository);
+            invoiceController.createInvoice();
+        }
+
+    }
+}
+
+
