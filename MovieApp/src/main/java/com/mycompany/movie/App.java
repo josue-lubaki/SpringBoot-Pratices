38b283f44ec6bc640465ae2f86db0e7bab0fb3b1
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-01 02:48:03 -0400

    deplacement AppConfig to App

--- MovieApp/src/main/java/com/mycompany/movie/App.java
+++ MovieApp/src/main/java/com/mycompany/movie/App.java
@@ -3,19 +3,27 @@ package com.mycompany.movie;
 import com.mycompany.movie.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Applicationn de demarrage par Inversion de contrôle
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        // Utilisation de l'ID du bean
+
+@Configuration
+@ComponentScan(
+    basePackages =
+        {
+            "com.mycompany.movie.service",
+            "com.mycompany.movie.controller",
+            "com.mycompany.movie.repository.file"
+        }
+)
+@PropertySource("classpath:application.properties")
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
--- invoice/src/main/java/com/mycompany/invoice/App.java
+++ invoice/src/main/java/com/mycompany/invoice/App.java
@@ -3,8 +3,9 @@ package com.mycompany.invoice;
 import com.mycompany.invoice.controller.InvoiceControllerInterface;
 import com.mycompany.invoice.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoice.service.InvoiceServiceInterface;
+import com.mycompany.invoice.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
@@ -12,11 +13,27 @@ import java.util.Scanner;
 /**
  * Classe de lancement d'Application
  */
+
+@Configuration
+@ComponentScan(
+    basePackages =
+        {
+            "com.mycompany.invoice.controller.web",
+            "com.mycompany.invoice.repository.database",
+            /*"com.mycompany.invoice.service.prefix"*/
+        }
+)
+@PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceController =  context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
+    }
+
+    @Bean
+    public InvoiceServiceInterface configureInvoiceService(){
+        return new InvoiceServicePrefix();
     }
 }
 
