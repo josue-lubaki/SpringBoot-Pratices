f70a642c434195ed99841047dee5084a52f52054
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-01 02:27:13 -0400

    liberation du fichier xml vers AppConfig

--- invoice/src/main/java/com/mycompany/invoice/App.java
+++ invoice/src/main/java/com/mycompany/invoice/App.java
@@ -4,6 +4,7 @@ import com.mycompany.invoice.controller.InvoiceControllerInterface;
 import com.mycompany.invoice.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoice.service.InvoiceServiceInterface;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
@@ -13,8 +14,7 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        // instancier une application contexte (conteneur leger) sur la base d'un fichier XML qui se trouve sur mon ClassPath
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         InvoiceControllerInterface invoiceController =  context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
     }
