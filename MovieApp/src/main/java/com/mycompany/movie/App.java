4ef253e8f2cea492104cf93c33b16dd49584182e
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-01 03:13:09 -0400

    exercice switch to SpringApplication

--- MovieApp/src/main/java/com/mycompany/movie/App.java
+++ MovieApp/src/main/java/com/mycompany/movie/App.java
@@ -1,29 +1,18 @@
 package com.mycompany.movie;
 
 import com.mycompany.movie.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Applicationn de demarrage par Inversion de contrôle
  */
 
-@Configuration
-@ComponentScan(
-    basePackages =
-        {
-            "com.mycompany.movie.service",
-            "com.mycompany.movie.controller",
-            "com.mycompany.movie.repository.file"
-        }
-)
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
