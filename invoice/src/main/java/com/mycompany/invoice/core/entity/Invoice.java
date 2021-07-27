595c8c7860947122ccbecaf72b3638ec96e55204
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-27 03:05:56 -0400

    change jdbc Data to jpa Data

--- invoice/src/main/java/com/mycompany/invoice/core/entity/Invoice.java
+++ invoice/src/main/java/com/mycompany/invoice/core/entity/Invoice.java
@@ -1,11 +1,13 @@
 package com.mycompany.invoice.core.entity;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 
+import javax.persistence.*;
+
+@Entity
 public class Invoice {
     @Id
-    @Column("INVOICE_NUMBER")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="INVOICE_NUMBER")
     private String number;
     private String customerName;
     private String orderNumber;
